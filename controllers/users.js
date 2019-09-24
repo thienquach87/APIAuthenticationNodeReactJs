ee2188d50c91639b1063cc08cbd857606549bbe5
SPNthienquach <thien.quach@symphony.com>
2019-09-24 09:04:20 +0700

    Server-Side Validation

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,9 @@
 module.exports = {
     signUp: async (req, res, next) => {
+
+        console.log('contents of req.value.body', req.value.body);
         console.log('UserController.signUp() called!');
+        next();
     }, 
 
     signIn: async (req, res, next) => {
